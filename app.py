6df1373ac448f52c2232a958247965ad83ecf2a4
deepgr8 <73525616+deepgr8@users.noreply.github.com>
2025-02-16 23:33:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ config = {
     "measurementId": "G-XC05M13SYK"
 }
 
-cred = credentials.Certificate("/Users/deepuprajapati/Documents/Deepu_Python_projects/smartShare/Smartshare/nyaysetu-c6500-firebase-adminsdk-jr9vt-288feca55d.json")
+cred = credentials.Certificate("nyaysetu-c6500-firebase-adminsdk-jr9vt-288feca55d.json")
 firebase = firebase_admin.initialize_app(cred, {
     'databaseURL': config['databaseURL'],
     'storageBucket': config['storageBucket']
